004f15339e678830d1fa080995e1a1dc7cbd5d16
causedprince <106291344+causedprince@users.noreply.github.com>
2022-05-25 19:41:36 -0700

    Update 3.exercise.4.cpp
    Proposed alternative solution

--- ch03/3.exercise.4.cpp
+++ ch03/3.exercise.4.cpp
@@ -1,18 +1,24 @@
-#include "std_lib_facilities.h"
+#include <iostream>
+#include <cmath>
+#include <stdlib.h>
+
+
+
+using namespace std;
 
 int main()
 {
-    cout << "Enter two integer values: ";
-    int val1 {0};
-    int val2 {0};
-    cin >> val1 >> val2;
-    if (val1 < val2)
-        cout << "The smaller is " << val1 << " and the larger is " << val2 << ".\n";
-    else
-        cout << "The smaller is " << val2 << " and the larger is " << val1 << ".\n";
-    cout << val1 << " + " << val2 << " = " << val1+val2 << '\n';
-    cout << val1 << " - " << val2 << " = " << val1-val2 << '\n';
-    cout << val1 << " * " << val2 << " = " << val1*val2 << '\n';
-    cout << val1 << " / " << val2 << " = " << val1/val2 << '\n';
+  cout << "Enter two integer values: \n";
+  int val1;
+  int val2;
+  cin >> val1 >> val2;
+  cout << "sum of val1 and val2 = " << val1+val2;
+  cout << "\ndifference of val1 and val2 and vice versa = " << val1-val2 << "," << val2-val1;
+  cout << "\nproduct of val1 and val2 = " << val1*val2;
+  cout << "\nratio of val1 and val2 and vice versa = " << val1/val2 << "," << val2/val1;
+  cout << "\nlarger of both values = " << max(val1,val2);
+  cout << "\nsmaller of both values = " << min(val1,val2);
+
+
+return 0;
 }
-    
